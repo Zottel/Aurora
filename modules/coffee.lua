f83db32490d6a4608cf8d5965f666e37f0c44629
stephan <stephan@splatz1.(none)>
2010-05-31 21:10:37 +0200

    added {drink}+=n

--- modules/coffee.lua
+++ modules/coffee.lua
@@ -36,13 +36,14 @@ function interface.handlers.privmsg(network, sender, channel, message)
 	--print("++", pcre.match (message, "^([^ \\+]+)\\+\\+$"))
 	local drink_orig = pcre.match (message, "^([^ \\+]+)\\+\\+$") 
 	local new_drink = pcre.match(message, "drinks\.new\\(([^\\)\\+ ]+)\\)")
+	local incr_drink = pcre.match(message, "^([^ \\+]+\\+=\\d*)")
 	if drink_orig then
 		drink = string.lower(drink_orig)
 		if coffee.db[drink] then
 			if coffee.db[drink][string.lower(sender.nick)] then
 				coffee.db[drink][string.lower(sender.nick)] = 1 + coffee.db[drink][string.lower(sender.nick)]
 				network.send("PRIVMSG", channel, sender.nick .. " hatte schon " .. coffee.db[drink][string.lower(sender.nick)] .. " " .. drink_orig .. ".")
-				else
+			else
 				coffee.db[drink][string.lower(sender.nick)] = 1 
 				network.send("PRIVMSG", channel, "Wie putzig, " .. sender.nick .. " fängt an mit " .. drink_orig .. ".")
 			end
@@ -57,6 +58,21 @@ function interface.handlers.privmsg(network, sender, channel, message)
 			write_coffee_db()
 		else
 			network.send("privmsg", channel, "Error in coffee.lua: Drink exists!           Stack traceback: coffee, beer, mate, baileys…")
+		end
+	elseif incr_drink then
+		local incr_drink_name = pcre.match(incr_drink, "^([^ \\+]+)\\+=.*")
+		local incr_drink_number = tonumber(pcre.match(incr_drink, "^[^\\d]*([\\d]*)$"))
+		if coffee.db[incr_drink_name] then
+			if coffee.db[incr_drink_name][string.lower(sender.nick)] then
+				coffee.db[incr_drink_name][string.lower(sender.nick)] = incr_drink_number + coffee.db[incr_drink_name][string.lower(sender.nick)]
+				network.send("PRIVMSG", channel, sender.nick .. " hatte schon " .. coffee.db[incr_drink_name][string.lower(sender.nick)] .. " " .. incr_drink_name .. ".")
+			else
+				coffee.db[incr_drink_name][string.lower(sender.nick)] = incr_drink_number 
+				network.send("PRIVMSG", channel, "Wie putzig, " .. sender.nick .. " fängt an mit " .. incr_drink_name .. ".")
+			end
+			write_coffee_db()
+		else
+			network.send("privmsg", channel, "Error in coffee.lua: Drink does not exist!!  Stack traceback: coffee, beer, mate, baileys…")
 		end
 	end
 end
