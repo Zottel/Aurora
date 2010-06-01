8871125b64ee3eea719a49e1ca43d0bbbd56c84b
Simon Birnbach <simon@simon-birnbach.de>
2010-06-01 20:56:42 +0200

    Use tabs as indention

--- modules/events.lua
+++ modules/events.lua
@@ -44,34 +44,34 @@ end
 
 
 function interface.init(filename)
-    if not type(filename) == "string" then
-        return nil, "Error in events.lua: Please call with db filename."
-    end
+	if not type(filename) == "string" then
+		return nil, "Error in events.lua: Please call with db filename."
+	end
 	events.filename = filename
 	read_events_db()
 	return true
 end
 
 function interface.handlers.privmsg(network, sender, channel, message)
-    local help, command = pcre.match (message, "(^!help events(?: ([^ ]+)|))")
-		local new_event = pcre.match (message, "^!new_event ") 
-    local event_in = pcre.match (message, "^!in ")
-    local event_out = pcre.match (message, "^!out ")
+	local help, command = pcre.match (message, "(^!help events(?: ([^ ]+)|))")
+	local new_event = pcre.match (message, "^!new_event ") 
+	local event_in = pcre.match (message, "^!in ")
+	local event_out = pcre.match (message, "^!out ")
 
-    if help then
-        if command == "new_event" then
-            network.send("privmsg", channel, "To add a new event use: !new_event <date> <time> <name> <location> <description>")
-            network.send("privmsg", channel, "Example: !new_event 11.06.2010 19:00 \"Chaostreff\" \"TU 48-462\" \"Nächster Chaostreff\" ")
-        elseif command == "in" then
-            network.send("privmsg", channel, "To confirm that you are attending an event use: !in <name> [<date> [<time>]] [location]")
-            network.send("privmsg", channel, "Example: !in \"Chaostreff\" 11.06.2010 19:00 \"TU 48-462\" ")
-        elseif command == "out" then
-            network.send("privmsg", channel, "To deny that you are attending an event use: !out <name> [<date> [<time>]] [location]")
-            network.send("privmsg", channel, "Example: !out \"Chaostreff\" 11.06.2010 19:00 \"TU 48-462\" ")
-        else
-            network.send("privmsg", channel, "Supported commands: new_event, in, out. See !help events <command> for further details.")
-        end
-    end
+	if help then
+		if command == "new_event" then
+			network.send("privmsg", channel, "To add a new event use: !new_event <date> <time> <name> <location> <description>")
+			network.send("privmsg", channel, "Example: !new_event 11.06.2010 19:00 \"Chaostreff\" \"TU 48-462\" \"Nächster Chaostreff\" ")
+		elseif command == "in" then
+			network.send("privmsg", channel, "To confirm that you are attending an event use: !in <name> [<date> [<time>]] [location]")
+			network.send("privmsg", channel, "Example: !in \"Chaostreff\" 11.06.2010 19:00 \"TU 48-462\" ")
+		elseif command == "out" then
+			network.send("privmsg", channel, "To deny that you are attending an event use: !out <name> [<date> [<time>]] [location]")
+			network.send("privmsg", channel, "Example: !out \"Chaostreff\" 11.06.2010 19:00 \"TU 48-462\" ")
+		else
+			network.send("privmsg", channel, "Supported commands: new_event, in, out. See !help events <command> for further details.")
+		end
+	end
 end
 
 return interface
