a974295096987ed3eda209368c053063c7bb97a8
Julius Roob <julius@juliusroob.de>
2010-06-01 22:37:16 +0800

    [events] Regexeration…

--- modules/events.lua
+++ modules/events.lua
@@ -53,8 +53,8 @@ function interface.init(filename)
 end
 
 function interface.handlers.privmsg(network, sender, channel, message)
-    local help, command = pcre.match (message, "^!help events(?: ([^ ]+)|)")
-	local new_event = pcre.match (message, "^!new_event ") 
+    local help, command = pcre.match (message, "(^!help events(?: ([^ ]+)|))")
+		local new_event = pcre.match (message, "^!new_event ") 
     local event_in = pcre.match (message, "^!in ")
     local event_out = pcre.match (message, "^!out ")
 
